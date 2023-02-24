e71e2037651af07431af5f939796c4f74c7aa723
Diego-GomezCordova <diegogomez631@gmail.com>
2023-02-23 22:49:51 -0600

    Lower Tangent function 1.0

--- Project1/ConvexHull.py
+++ Project1/ConvexHull.py
@@ -35,14 +35,63 @@ class ConvexHull:
         for i in range(len(sub_convexB)):
             print(sub_convexB[i])
         
-        return []; 
-        #self.computeHull(self, sub_convexA, sub_convexB)
+        self.computeHull(sub_convexA, sub_convexB)
+
+        return[]
 
 
     # o(n)
     def computeHull(self, arrayA, arrayB):
         print(arrayA, arrayB)
+        #Testing lowerTangent()
+        if len(arrayA)==0:
+            return
+
+        print('Testing lowerTangent():')
+        lowerTan = self.lowerTangent(arrayA, arrayB)
+        print('P: ', lowerTan.p1, ' Q: ', lowerTan.p2, ' eq: ', lowerTan)
 
     def pointIsAboveLine(self, line: Line, point: Point):
         yLine = float(Line.getSlope(line) * Point.getX(point) + Line.getY_Intercept(line))
         return bool(Point.getY(point) > yLine)
+
+    #Function for finding the lower tangent
+    #Returns a Line object
+    def lowerTangent(self, pointsA: list, pointsB: list):
+
+        #P is the right-most point of A
+        print(len(pointsA)-1)
+        pointP = pointsA[len(pointsA)-1]
+        #Q is the left-most point of B
+        pointQ = pointsB[0]
+
+        #Fix point P
+        linePQ = Line(pointP, pointQ)
+        indexQNeighbor = 1
+        QNeighbor = pointsB[indexQNeighbor]
+        while self.pointIsAboveLine(linePQ,QNeighbor)==False:
+            pointQ = pointsB[indexQNeighbor]
+            linePQ = Line(pointP, pointQ)
+            indexQNeighbor += 1
+            if indexQNeighbor>=len(pointsB):
+                break
+            QNeighbor = pointsB[indexQNeighbor]
+        pointT2 = pointQ
+
+        #Fix point Q
+        pointQ = pointsB[0]
+        linePQ = Line(pointP, pointQ)
+        indexPNeighbor = len(pointsA)-2
+        PNeighbor = pointsA[indexPNeighbor]
+        while self.pointIsAboveLine(linePQ,PNeighbor)==False:
+            pointP = pointsA[indexPNeighbor]
+            linePQ = Line(pointP, pointQ)
+            indexPNeighbor -= 1
+            if indexPNeighbor<0:
+                break
+            PNeighbor = pointsA[indexPNeighbor]
+        pointT1 = pointP
+
+        #Return lower tangent line
+        return Line(pointT1, pointT2)
+
